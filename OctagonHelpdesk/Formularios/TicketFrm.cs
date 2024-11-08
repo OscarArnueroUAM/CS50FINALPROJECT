6df2b29122daffcd8aac363c6656a6cb8341c927
Fletcher17b <oiramgrillol.2@gmail.com>
2024-11-07 23:18:28 -0600

    Commit de Harvey

---
+++ OctagonHelpdesk/Formularios/TicketFrm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OctagonHelpdesk.Models;
+
+namespace OctagonHelpdesk.Formularios
+{
+    public partial class TicketFrm : Form
+    {
+        public TicketFrm()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.TicketFrm_Load);
+            CrearNuevoTicket();
+        }
+
+        private void CrearNuevoTicket()
+        {
+            int empleadoID = Sesion.EmpleadoID;
+            Ticket nuevoTicket = new Ticket(empleadoID);
+            txtCreadorPor.Text = nuevoTicket.CreadorPor.ToString();
+        }
+
+        private void TicketFrm_Load(object sender, EventArgs e)
+        {
+            CrearNuevoTicket();
+        }
+    }
+
+}
+
---
+++ OctagonHelpdesk/Models/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctagonHelpdesk.Models
+{
+    internal class Ticket
+    {
+        public int TicketID { get; set; }
+        public int CreadorPor { get; private set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public Ticket(int creadorPorID)
+        {
+            CreadorPor = creadorPorID;
+            FechaCreacion = DateTime.Now;
+        }
+
+    }
+
+}
