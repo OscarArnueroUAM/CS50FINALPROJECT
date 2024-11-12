4d1e0cbb8440f00dece46ca9f07d481ef04b9a0e
AliciaEA <aliestrada@uamv.edu.ni>
2024-11-12 14:16:58 -0600

    Se hizo un evento para pasar los datos del ticket creado a la lista, El ID no se autogenera correctamente...

--- OctagonHelpdesk/Formularios/RegTicketFrm.cs
+++ OctagonHelpdesk/Formularios/RegTicketFrm.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OctagonHelpdesk.Models;
+using OctagonHelpdesk.Services;
 
 
 namespace OctagonHelpdesk.Formularios
 {
     public partial class RegTicketFrm : Form
     {
+        public TicketService tickets = new TicketService();
+        public Ticket ticketSel = new Ticket();
         public RegTicketFrm()
         {
             InitializeComponent();
@@ -22,7 +26,20 @@ namespace OctagonHelpdesk.Formularios
         private void BtnCreateTicket_Click(object sender, EventArgs e)
         {
             CmpTicketFrm ticketFrm = new CmpTicketFrm();
+            ticketFrm.TicketCreated += OnTicketCreated;
             ticketFrm.ShowDialog();
+            MostrarTickets();
+
+        }
+        private void OnTicketCreated(Ticket ticket)
+        {
+            tickets.AddTicket(ticket);
+        }
+
+        private void MostrarTickets()
+        {
+            DgvRegTickets.DataSource = null;
+            DgvRegTickets.DataSource = tickets.GetTickets();
         }
     }
 }
--- OctagonHelpdesk/Services/TicketService.cs
+++ OctagonHelpdesk/Services/TicketService.cs
@@ -7,41 +7,42 @@ using System.Threading.Tasks;
 
 namespace OctagonHelpdesk.Services
 {
-    internal class TicketService
+    public class TicketService
     {
-        public List<Ticket> tickets = new List<Ticket>();
+        public List<Ticket> Tickets = new List<Ticket>();
 
         //Añado el ticket a la lista de tickets
         public void AddTicket(Ticket ticket)
         {
-            tickets.Add(ticket);
+            Tickets.Add(ticket);
         }
 
         //Para eliminar, busco su posicion y solo desactivo o inhabilito el estado del ticket
         public void RemoveTicket(Ticket ticket)
         {
             int position = FindPosition(ticket.IDTicket);
-            tickets[position].ActiveState = false;
+            Tickets[position].ActiveState = false;
         }
         
         //Para actualizar, busco la posición del ticket y lo actualizo
         public void UpdateTicket(Ticket ticket)
         {
             int position = FindPosition(ticket.IDTicket);
-            tickets[position] = ticket;
+            Tickets[position] = ticket;
         }
         
         //Busco la posicion del ticket en la lista
         public int FindPosition(int IDTicket)
         {
-            int position = tickets.FindIndex(ticket => ticket.IDTicket == IDTicket);
+            int position = Tickets.FindIndex(ticket => ticket.IDTicket == IDTicket);
             return position;
         }
 
         //Retorno la lista de tickets
         public List<Ticket> GetTickets()
         {
-            return tickets;
+            return Tickets;
         }
+
     }
 }
